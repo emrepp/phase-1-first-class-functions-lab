bc343edcc763d967b4534a9b7086e10973806fa2
Emma Repp <emmarepp.draws@gmail.com>
2022-06-10 20:48:13 -1000

    commit tree 9c6acf09ac89db470153dc8cf03da53fcf0788e2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers= function(drivers1) {
+return drivers1.slice(0,2);
+}
+
+const returnLastTwoDrivers= function(drivers2) {
+    return drivers2.slice(2,5);
+}
+    
+let selectingDrivers = 
+    [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+    const createFareMultiplier = function(n) {
+        return function(fare) {
+        return fare * n;
+    } }
+
+    const fareDoubler = createFareMultiplier(2)
+    const fareTripler = createFareMultiplier(3)
+    
+    const selectDifferentDrivers = function (arrayOfDrivers, 
+        returnFirstTwoDrivers)
+    {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    }
